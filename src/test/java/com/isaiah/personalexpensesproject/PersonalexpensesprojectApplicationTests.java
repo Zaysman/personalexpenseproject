9952c05665ed64ad773b05a7205501513d6445e8
Isaiah <i.x.smith137@gmail.com>
2024-11-20 19:47:14 -0500

    Initial Commit from local repository

---
+++ src/test/java/com/isaiah/personalexpensesproject/PersonalexpensesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiah.personalexpensesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalexpensesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
